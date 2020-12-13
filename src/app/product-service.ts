ae804be2b7199d8b6d5c71a6ad9b5e30cb81f3a7
girish shelke <govindshelke29081996@gmail.com>
2020-12-13 14:27:56 +0530

    angular app

---
+++ src/app/product-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+
+@Injectable()
+export class ProductService {
+
+  url = 'http://localhost:4000/product'
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProducts() {
+    return this.http.get(this.url)
+  }
+}
